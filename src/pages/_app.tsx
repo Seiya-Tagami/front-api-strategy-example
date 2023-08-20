0b887d6d0111bcf6fb4f5b6758160bf3ba39f812
naruto1031 <naruto0614@au.com>
2023-08-20 17:02:43 +0900

    フロントエンド側のNextAuth仮実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import { queryClient } from '@/lib/api-client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   if (process.env.NEXT_PUBLIC_MOCKING === 'enabled') {
     require('../lib/mock')
   }
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <SessionProvider session={pageProps.session}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </SessionProvider>
   )
 }
